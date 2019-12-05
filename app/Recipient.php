512b12bab73d44de2a3f4686dafd515549fd067f
tartakovskyi <vvt2001@ukr.net>
2019-12-05 23:41:34 +0200

    query

--- app/Recipient.php
+++ app/Recipient.php
@@ -7,13 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Recipient extends Model
 {
 	public function getRecipients () {
-		$recipients = $this->select('email', 'first_name', 'last_name', 'group_id', 'status')->get()->toArray();
-		foreach (&$recipients as $recipient) {
-			$recipient['name'] = $recipient['last_name'].', '.$recipient['first_name'];
-			unset($recipient['last_name']);
-			unset($recipient['first_name']);
-		}
-		return $recipients;
+		return $this->leftJoin('groups', 'recipients.group_id', '=', 'groups.id')
+		->select('recipients.id', 'email', 'first_name', 'last_name', 'group_id', 'status', 'name')
+		->get()
+		->toArray();
 	}
-    
 }
--- resources/views/recipients.blade.php
+++ resources/views/recipients.blade.php
@@ -4,11 +4,11 @@
 <table>
     @foreach ($recArr as $recipient)
     <tr>
-        @foreach ($recipient as $prop)
-        <td>
-            {{$prop}}
-        </td>
-        @endforeach 
+        <td>{{$recipient['email']}}</td>
+        <td>{{$recipient['last_name']}}, {{$recipient['first_name']}}</td>
+        <td>{{$recipient['name']}}</td>
+        <td>{{$recipient['status']}}</td>
+        <td><a href="/recipient/{{$recipient['id']}}/edit/">edit</a></td>        
     </tr>
     @endforeach 
 </table>
