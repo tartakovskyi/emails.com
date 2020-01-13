3c81841be0ff4a6440f34a6a8f587eafe9a4f860
tartakovskyi <vvt2001@ukr.net>
2020-01-13 21:24:07 +0200

    big fix

--- public/js/app.js
+++ public/js/app.js
@@ -37086,15 +37086,15 @@ var ajax = function ajax(btnID, entityData) {
 
   switch (btnID) {
     case 'saveBtn':
-      url = '/api/' + entity + '/save/';
+      url = '/api/' + entity + '/save';
       break;
 
     case 'updateBtn':
-      url = '/api/' + entity + '/save/' + entityData.id + '/';
+      url = '/api/' + entity + '/save/' + entityData.id;
       break;
 
     case 'deleteBtn':
-      url = '/api/' + entity + '/delete/' + entityData.id + '/';
+      url = '/api/' + entity + '/delete/' + entityData.id;
   }
 
   var response = axios.post(url, entityData).then(function (response) {
