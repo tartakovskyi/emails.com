3b5508a3e4483b872312d581e0cb08b23e017e54
tartakovskyi <vvt2001@ukr.net>
2020-01-31 23:37:51 +0200

    user crud

--- app/User.php
+++ app/User.php
@@ -37,12 +37,4 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-
-    public static function saveUser($data, $id = null)
-    {
-
-        $recipient = self::find($id);
-        $recipient->fill($data);
-        $recipient->save();
-    }
 }
